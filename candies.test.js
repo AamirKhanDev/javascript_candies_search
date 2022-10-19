d56812338134fa02614ec31e9ea53c3f3a3b506c
Aamir Khan <63204104+HoodieB@users.noreply.github.com>
2022-10-19 19:13:02 +0100

    Completed candies search challenge

---
+++ candies.test.js
@@ -0,0 +1,19 @@
+const searchCandies = require('./candies');
+
+describe( 'searchCandies', () => {
+  it( 'returns the expected result searching with arguments ("Ma",10)', () => {
+    expect(searchCandies('Ma', 10)).toEqual([ 'Mars', 'Maltesers' ]);
+  });
+  it( 'returns the expected result searching with arguments ("Ma",2)', () => {
+    expect(searchCandies('Ma', 2)).toEqual([ 'Mars' ]);
+  });
+  it( 'returns the expected result searching with arguments ("S",10)', () => {
+    expect(searchCandies('S', 10)).toEqual([ 'Skitties', 'Skittles', 'Starburst' ]);
+  });
+  it( 'returns the expected result searching with arguments ("S",4)', () => {
+    expect(searchCandies('S', 4)).toEqual([ 'Skitties', 'Skittles' ]);
+  });
+  it( 'returns the expected result searching with arguments ("ma",10)', () => {
+    expect(searchCandies('ma', 10)).toEqual([ 'Mars', 'Maltesers' ]);
+  });
+});
